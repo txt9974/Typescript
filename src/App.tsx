eb0a859d1ef6185ee49310a824fc8a606eb8be5f
txt9974 <thangtxph39417@fpt.edu.vn>
2024-03-20 17:31:49 +0700

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Footer from './components/Footer'
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import ProductDetail from './pages/ProductDetail'
