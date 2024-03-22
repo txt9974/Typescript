cced297ee2179a76273fa897f43ccb3f4a094cb3
txt9974 <thangtxph39417@fpt.edu.vn>
2024-03-22 14:50:46 +0700

    holla

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -31,7 +31,7 @@ function Header() {
         <Navbar.Collapse id='navbarScroll'>
           <Nav className='me-auto my-2 my-lg-0' style={{ maxHeight: '100px' }} navbarScroll>
             <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='#action2'>Link</Nav.Link>
+            <Nav.Link href='*'>Link</Nav.Link>
             <NavDropdown title='Link' id='navbarScrollingDropdown'>
               <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
               <NavDropdown.Item href='#action4'>Another action</NavDropdown.Item>
